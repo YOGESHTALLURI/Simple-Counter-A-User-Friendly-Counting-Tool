cad0024920aa60f9049c469ee32d3dd9264ac812
YOGESHTALLURI <yogeshthalluri@gmail.com>
2024-04-23 11:21:02 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,19 @@
+ let count=0;
+ function increment(){
+     count++;
+     document.getElementById("Count").textContent=count;
+     
+ }
+ function reset(){
+    count=0;
+     document.getElementById("Count").textContent=count;
+    
+ }
+
+ function decrement(){
+    count--;
+    document.getElementById("Count").textContent=count;
+ }
+
+
+ 
